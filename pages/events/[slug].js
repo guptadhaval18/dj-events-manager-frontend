9da77d39a6f245accb2647b2406bc3ce1283dbc8
guptadhaval18 <guptadhaval18@gmail.com>
2021-10-23 15:47:44 +0530

    Connect to strapi API

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -24,12 +24,16 @@ export default function EventPage({ evt }) {
         </div>
 
         <span>
-          {evt.date} at {evt.time}
+          {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
         </span>
         <h1>{evt.name}</h1>
         {evt.image && (
           <div className={styles.image}>
-            <Image src={evt.image} width={960} height={600} />
+            <Image
+              src={evt.image.formats.medium.url}
+              width={960}
+              height={600}
+            />
           </div>
         )}
 
@@ -48,11 +52,11 @@ export default function EventPage({ evt }) {
   )
 }
 
-//getStaticPaths tells getStaticProps which all paths can occur 
+//getStaticPaths tells getStaticProps which all paths can occur
 //during static deployment.
 //fallback tells what to do when new path occurs during deployment.
 export async function getStaticPaths() {
-  const res = await fetch(`${API_URL}/api/events`)
+  const res = await fetch(`${API_URL}/events`)
   const events = await res.json()
 
   const paths = events.map((evt) => ({
@@ -66,7 +70,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const res = await fetch(`${API_URL}/api/events/${slug}`)
+  const res = await fetch(`${API_URL}/events?slug=${slug}`)
   const events = await res.json()
 
   return {
