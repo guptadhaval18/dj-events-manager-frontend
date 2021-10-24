c962d37ad2cd77eb5f9a16a6ecfa2c13f77cea82
guptadhaval18 <guptadhaval18@gmail.com>
2021-10-24 12:52:55 +0530

    save httpOnly cookie

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -177,7 +177,7 @@ export default function EditEventPage({ evt }) {
   )
 }
 
-export async function getServerSideProps({ params: { id } }) {
+export async function getServerSideProps({ params: { id } }, req) {
   const res = await fetch(`${API_URL}/events/${id}`)
   const evt = await res.json()
 
