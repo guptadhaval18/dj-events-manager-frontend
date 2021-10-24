059bb9c6433398695a639b17adc9bedf7ebb8b58
guptadhaval18 <guptadhaval18@gmail.com>
2021-10-24 13:03:34 +0530

    Persist user

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -8,6 +8,10 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
   const [error, setError] = useState(null)
 
+  const router = useRouter()
+
+  useEffect(() => checkUserLoggedIn(), [])
+
   // Register user
   const register = async (user) => {
     console.log(user)
@@ -32,6 +36,7 @@ export const AuthProvider = ({ children }) => {
 
     if (res.ok) {
       setUser(data.user)
+      router.push('/account/dashboard')
     } else {
       setError(data.message)
       setError(null)
@@ -45,7 +50,14 @@ export const AuthProvider = ({ children }) => {
 
   // Check if user is logged in
   const checkUserLoggedIn = async (user) => {
-    console.log('Check')
+    const res = await fetch(`${NEXT_URL}/api/user`)
+    const data = await res.json()
+
+    if (res.ok) {
+      setUser(data.user)
+    } else {
+      setUser(null)
+    }
   }
 
   return (
