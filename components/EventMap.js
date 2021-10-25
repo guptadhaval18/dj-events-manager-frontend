043cbce8964753d9261002fc7e820eb308f0ab15
guptadhaval18 <guptadhaval18@gmail.com>
2021-10-25 13:57:08 +0530

    Add map to event page

---
+++ components/EventMap.js
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+import { useState, useEffect } from 'react'
+import ReactMapGl, { Marker } from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+import Geocode from 'react-geocode'
+
+export default function EventMap({ evt }) {
+  const [lat, setLat] = useState(28.7041)
+  const [lng, setLng] = useState(77.1025)
+  const [loading, setLoading] = useState(true)
+  const [viewport, setViewport] = useState({
+    latitude: 28.7041,
+    longitude: 77.1025,
+    width: '100%',
+    height: '500px',
+    zoom: 12,
+  })
+
+  useEffect(() => {
+    // Get latitude & longitude from address.
+    // Geocode.fromAddress(evt.address).then(
+    //   (response) => {
+    //     const { lat, lng } = response.results[0].geometry.location
+    //     setLat(lat)
+    //     setLng(lng)
+    //     setViewport({ ...viewport, latitude: lat, longitude: lng })
+    //     setLoading(false)
+    //   },
+    //   (error) => {
+    //     console.error(error)
+    //   }
+    // )
+    setLoading(false)
+  }, [])
+
+  Geocode.setApiKey(process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY)
+
+  if (loading) return false
+
+  return (
+    <ReactMapGl
+      {...viewport}
+      mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
+      onViewportChange={(vp) => setViewport(vp)}
+    >
+      <Marker key={evt.id} latitude={lat} longitude={lng}>
+        <Image src='/images/pin.svg' width={30} height={30} />
+      </Marker>
+    </ReactMapGl>
+  )
+}
